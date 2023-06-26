6edd9cf6bd3ac297862ff0f2a0542eb9e738b90a
Yakov Zhurkin <zh4rk1n@yandex.ru>
2023-06-27 00:08:01 +0300

    HW4

---
+++ task25/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int findStepen_B_otA(int num1, int num2)
+{
+    
+    int res = 1;
+    for(int i = 0;i<num2;i++)
+    {
+        res*=num1;
+    }
+    return res;
+    
+}
+
+
+
+int number1 = Prompt("введите число A(основание) :");
+int number2 = Prompt("Введите число B(показатель) :");
+if (number2>-1)
+   {
+int result = findStepen_B_otA(number1,number2);
+Console.WriteLine("Число A в степени B  = " + result);
+   }
+   else
+{
+    Console.WriteLine("Показатель степени отрицательный!!!");
+}
+
---
+++ task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(String message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int sumCifr(int num)
+{
+    int sum = 0;
+    while(num>0)
+    {
+        sum+=num%10;
+        num/=10;
+    }
+    return sum;
+}
+
+
+int number = Prompt("Введите число : ");
+int result = sumCifr(number);
+Console.WriteLine($"Сумма цифр числа {number} = {result}");
